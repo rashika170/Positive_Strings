420e241aa07fda34f979315bc224001d19a5cdc4
rashika170 <rashika162001@gmail.com>
2022-06-14 20:27:28 +0530

    Added list of Affirmations in datasource.kt

---
+++ app/src/main/java/com/example/positive_strings/data/datasource.kt
@@ -0,0 +1,22 @@
+package com.example.positive_strings.data
+
+import com.example.positive_strings.R
+import com.example.positive_strings.model.Affirmation
+
+class datasource {
+
+    fun loadAffirmations(): List<Affirmation> {
+        return listOf<Affirmation>(
+            Affirmation(R.string.affirmation1),
+            Affirmation(R.string.affirmation2),
+            Affirmation(R.string.affirmation3),
+            Affirmation(R.string.affirmation4),
+            Affirmation(R.string.affirmation5),
+            Affirmation(R.string.affirmation6),
+            Affirmation(R.string.affirmation7),
+            Affirmation(R.string.affirmation8),
+            Affirmation(R.string.affirmation9),
+            Affirmation(R.string.affirmation10)
+        )
+    }
+}
